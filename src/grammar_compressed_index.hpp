44fd0859c028dfa10a4422c38e8b12f39d125c6c
jamie <jinjiedeng.jjd@gmail.com>
2020-12-09 20:03:20 +0800

    update grammar_compressed_index.hpp parse text to get information required to construct (1) grammar_rules (2) non_terminal_text_index (fm_index on non_terminal_text)

--- src/grammar_compressed_index.hpp
+++ src/grammar_compressed_index.hpp
@@ -5,6 +5,27 @@
 #include <sdsl/int_vector.hpp>
 #include <sdsl/wavelet_trees.hpp>
 
+
+void construct_non_terminal_text_index
+(
+  csa_wt<wt_int<>> &non_terminal_text_index,
+  sdsl::int_vector<32> &non_terminal_text
+)
+{
+  sdsl::constrcut_im(non_terminal_text_index, non_terminal_text, 4);
+  return;
+}
+
+void construct_grammar_rules
+(
+  csa_wt<> &grammar_rules,
+  std::vector<sdsl::int_vector<8>> const &terminal_substring_vector
+)
+{
+  // TODO: constrcut grammar_rules by terminal_substring_vector
+  return;
+}
+
 class Grammar_Compressed_Index
 {
 public:
@@ -14,21 +35,22 @@ public:
     sdsl::int_vector<8> text;
     sdsl::load_vector_from_file(text, file, 1);
 
-    std::vector<sdsl::int_vector<8>> terminal_sequence_vector;
-    sdsl::int_vector<32> non_terminal_vector;
-    parse_text(text, terminal_sequence_vector, nontermial_text);
+    std::vector<sdsl::int_vector<8>> terminal_substring_vector;
+    sdsl::int_vector<32> nonterminal_vector;
+    parse_text(text, terminal_substring_vector, non_terminal_text);
 
-    construct(grammar_rules, terminal_sequence_vector);
-    construct(fm_index_nontermial_text, nontermial_text)
+    construct_grammar_rules(grammar_rules, terminal_substring_vector);
+    construct_non_terminal_text_index(non_terminal_text_index, non_terminal_text)
   }
 
   size_t count (sdsl::int_vector<8> const &pattern)
   {
+    // TODO: details of couting
     return 0;
   }
 
   csa_wt<> grammar_rules;
-  csa_wt<> fm_index_nontermial_text;
+  csa_wt<wt_int<>> non_terminal_text_index;
 };
 
 #endif
