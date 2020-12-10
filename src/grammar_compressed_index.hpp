7523f6a29d7e406a6ac95b875693b7d4e7b1fcb4
jamie <jinjiedeng.jjd@gmail.com>
2020-12-11 07:50:09 +0800

    move construction of (1) grammar_rule_set (2) grammar_compressed_text into parse_text

--- src/grammar_compressed_index.hpp
+++ src/grammar_compressed_index.hpp
@@ -1,56 +1,39 @@
 #ifndef GRAMMAR_COMPRESSED_INDEX_HPP_
 #define GRAMMAR_COMPRESSED_INDEX_HPP_
 
-#include <vector>
-#include <sdsl/int_vector.hpp>
 #include <sdsl/wavelet_trees.hpp>
 
-
-void construct_non_terminal_text_index
-(
-  csa_wt<wt_int<>> &non_terminal_text_index,
-  sdsl::int_vector<32> &non_terminal_text
-)
-{
-  sdsl::constrcut_im(non_terminal_text_index, non_terminal_text, 4);
-  return;
-}
-
-void construct_grammar_rules
-(
-  csa_wt<> &grammar_rules,
-  std::vector<sdsl::int_vector<8>> const &terminal_substring_vector
-)
-{
-  // TODO: constrcut grammar_rules by terminal_substring_vector
-  return;
-}
-
+template
+<
+  typename Text_Type,
+  typename Pattern_Type,
+  typename Grammar_Rule_Set_Type,
+  typename Grammar_Compressed_Text_Type
+>
 class Grammar_Compressed_Index
 {
 public:
 
+  using Size_Type = typename Text_Type::size_type;
+
   Grammar_Compressed_Index (char const *file)
   {
-    sdsl::int_vector<8> text;
+    Text_Type text;
     sdsl::load_vector_from_file(text, file, 1);
-
-    std::vector<sdsl::int_vector<8>> terminal_substring_vector;
-    sdsl::int_vector<32> nonterminal_vector;
-    parse_text(text, terminal_substring_vector, non_terminal_text);
-
-    construct_grammar_rules(grammar_rules, terminal_substring_vector);
-    construct_non_terminal_text_index(non_terminal_text_index, non_terminal_text)
+    sdsl::append_zero_symbol(text);
+    parse_text(text, grammar_rule_set, grammar_compressed_text);
   }
 
-  size_t count (sdsl::int_vector<8> const &pattern)
+  Size_Type count (Pattern_Type const &pattern)
   {
-    // TODO: details of couting
     return 0;
   }
 
-  csa_wt<> grammar_rules;
-  csa_wt<wt_int<>> non_terminal_text_index;
+private:
+
+  Grammar_Rule_Set_Type         grammar_rule_set;
+  Grammar_Compressed_Text_Type  grammar_compressed_text;
+
 };
 
 #endif
