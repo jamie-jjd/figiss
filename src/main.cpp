18a9e98c1ddf469b4b0fd3048ce2307335c0c2f6
jamie <jinjiedeng.jjd@gmail.com>
2020-12-14 20:09:40 +0800

    construct grammar trie

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <memory>
 #include <numeric>
 #include <stdexcept>
 #include <sdsl/suffix_trees.hpp>
@@ -6,84 +7,162 @@
 constexpr uint8_t S_TYPE {1};
 constexpr uint8_t L_TYPE {0};
 
-template <typename sl_type_vector>
+template <typename sl_type_vector_type>
 constexpr bool is_rightmost_l_type
 (
-  sl_type_vector const &sl_type_vector_,
-  uint64_t const &position
+  sl_type_vector_type const &sl_type_vector,
+  uint64_t const position
 )
 {
   return
   (
-    (sl_type_vector_[position] == L_TYPE)
+    (sl_type_vector[position] == L_TYPE)
     &&
-    (sl_type_vector_[position + 1] == S_TYPE)
+    (sl_type_vector[position + 1] == S_TYPE)
   );
 }
 
-template <typename sl_type_vector>
+template <typename sl_type_vector_type>
 constexpr bool is_leftmost_s_type
 (
-  sl_type_vector const &sl_type_vector_,
-  uint64_t const &position
+  sl_type_vector_type const &sl_type_vector,
+  uint64_t const position
 )
 {
   return
   (
-    (sl_type_vector_[position] == S_TYPE)
+    (sl_type_vector[position] == S_TYPE)
     &&
-    (sl_type_vector_[position - 1] == L_TYPE)
+    (sl_type_vector[position - 1] == L_TYPE)
   );
 }
 
 template
 <
-  typename text,
-  typename character_bucket_range_vector
+  typename text_type,
+  typename character_bucket_range_vector_type
 >
 void calculate_cumulative_character_bucket_end
 (
-  text const &text_,
-  character_bucket_range_vector &character_bucket_range_vector_
+  text_type const &text,
+  character_bucket_range_vector &character_bucket_range_vector
 )
 {
   std::fill
   (
-    character_bucket_range_vector_.begin(),
-    character_bucket_range_vector_.end(),
+    character_bucket_range_vector.begin(),
+    character_bucket_range_vector.end(),
     0
   );
-  for (auto const &character : text_)
+  for (auto const &character : text)
   {
-    ++character_bucket_range_vector_[character];
+    ++character_bucket_range_vector[character];
   }
   std::partial_sum
   (
-    character_bucket_range_vector_.begin(),
-    character_bucket_range_vector_.end(),
-    character_bucket_range_vector_.begin()
+    character_bucket_range_vector.begin(),
+    character_bucket_range_vector.end(),
+    character_bucket_range_vector.begin()
   );
   return;
 }
 
 template
 <
-  typename text,
-  typename character_bucket_range_vector
+  typename text_type,
+  typename character_bucket_range_vector_type
 >
 void calculate_cumulative_character_bucket_begin
 (
-  text const &text_,
-  character_bucket_range_vector &character_bucket_range_vector_
+  text_type const &text,
+  character_bucket_range_vector_type &character_bucket_range_vector
 )
 {
-  calculate_cumulative_character_bucket_end(text_, character_bucket_range_vector_);
-  for (auto character {character_bucket_range_vector_.size() - 1}; character > 0; --character)
+  calculate_cumulative_character_bucket_end(text, character_bucket_range_vector);
+  for (auto character {character_bucket_range_vector.size() - 1}; character > 0; --character)
   {
-    character_bucket_range_vector_[character] = character_bucket_range_vector_[character - 1];
+    character_bucket_range_vector[character] = character_bucket_range_vector[character - 1];
   }
-  character_bucket_range_vector_[0] = 0;
+  character_bucket_range_vector[0] = 0;
   return;
+}
+
+template
+<
+  character_type,
+  size_type
+>
+struct grammar_rule_trie_node
+{
+  using range_type = std::pair<size_type, size_type>;
+
+  range_type edge_range;
+  std::map<character_type, std::shared_ptr<grammar_rule_trie_node>> branch;
+  range_type non_terminal_range;
+
+  grammar_rule_trie_node
+  (
+    range_type const &edge_range_
+  ) : edge_range {edge_range_}
+  {
+  }
+
+  grammar_rule_trie_node
+  (
+    range_type const &edge_range_
+    size_type const &non_terminal
+  ) : edge_range {edge_range_},
+      non_terminal_range {{non_terminal, non_terminal}}
+  {
+  }
+};
+
+template
+<
+  typename grammar_rule_trie_node_type,
+  typename grammar_rule_vector_type,
+  typename cumulative_position_type,
+  typename non_terminal_type
+>
+void insert_grammar_rule
+(
+  grammar_rule_trie_node_type root,
+  grammar_rule_vector_type const &grammar_rule_vector,
+  cumulative_position_type const begin,
+  cumulative_position_type const end,
+  non_terminal_type const non_terminal
+)
+{
+  auto node {root};
+  auto position {begin};
+  while (true)
+  {
+    auto character {grammar_rule_vector[position]};
+    auto child {node->branch[character]};
+    if (child != node->branch.end())
+    {
+      auto position_child {std::get<0>(child->edge_range)};
+      auto end_child {std::get<1>(child->edge_range)};
+      while
+      (
+        (position_child != end_child)
+        &&
+        (position != end)
+        &&
+        (grammar_rule_vector[position_child] == grammar_rule_vector[position])
+      )
+      {
+        ++position_child;
+        ++position;
+      }
+      // TODO
+    }
+    else
+    {
+      node->branch[character] = std::make_shared<grammar_rule_trie_node_type>({position, end}, non_terminal);
+      break;
+    }
+  }
 }
 
 int main (int argc, char **argv)
@@ -93,12 +172,12 @@ int main (int argc, char **argv)
     throw std::runtime_error(std::string("usage: ") + argv[0] + " [input path]");
   }
 
-  // read text from file
+  /* read text from file */
   sdsl::int_vector<8> text;
   sdsl::load_vector_from_file(text, argv[1], 1);
   sdsl::append_zero_symbol(text);
 
-  /* initialize sl_type_vector of text */
+  /* initialize SL-type of characters of text */
   sdsl::bit_vector sl_type_vector(text.size(), S_TYPE);
   for (auto position {text.size() - 1}; position > 0; --position)
   {
@@ -244,7 +323,7 @@ int main (int argc, char **argv)
   }
 
   /* collect characters of grammar-compressed text */
-  sdsl::int_vector<> grammar_compressed_text(begin_spare_space);
+  std::int_vector<> grammar_compressed_text(begin_spare_space);
   std::copy_if
   (
     std::next(text_position_vector.begin(), begin_spare_space),
@@ -256,10 +335,76 @@ int main (int argc, char **argv)
     }
   );
 
-  std::cout << grammar_compressed_text << '\n';
   /* construct FM-index on grammar-compressed text */
   sdsl::csa_wt<sdsl::wt_int<>> grammar_compressed_text_fm_index;
   sdsl::construct_im(grammar_compressed_text_fm_index, grammar_compressed_text);
+
+  /* collect beginning position of grammar rules in grammar rule set */
+  auto end_grammar_rule_begin_vector
+  {
+    0ULL
+    +
+    std::distance
+    (
+      std::stable_partition
+      (
+        text_position_vector.begin(),
+        (text_position_vector.begin() + begin_spare_space),
+        [&] (auto const &position)
+        {
+          return (position != text.size());
+        }
+      )
+    )
+  };
+
+  sdsl::int_vector<> grammar_rule_vector;
+
+  auto size_grammar_rule_vector {0ULL};
+  for (auto rank {0ULL}; rank < end_grammar_rule_begin_vector; ++rank)
+  {
+    auto begin_grammar_rule {static_cast<uint64_t>(text_position_vector[rank])};
+    auto offset {1ULL};
+    while (!is_leftmost_s_type(sl_type_vector, begin_grammar_rule + offset))
+    {
+      ++offset;
+    }
+    size_grammar_rule_vector += offset;
+  }
+
+  /* constrcut compact trie of grammar rule set */
+  std::shared_ptr<grammar_rule_trie_node> grammar_rule_trie_root;
+  std::shared_ptr<grammar_rule_trie_node> reverse_grammar_rule_trie_root;
+  grammar_rule_vector.resize(size_grammar_rule_vector);
+  auto cumulative_begin {0ULL};
+  for (auto rank {0ULL}; rank < end_grammar_rule_begin_vector; ++rank)
+  {
+    auto begin_grammar_rule {static_cast<uint64_t>(text_position_vector[rank])};
+    grammar_rule_vector[cumulative_begin] = text[begin_grammar_rule];
+    auto offset {1ULL};
+    while (!is_leftmost_s_type(sl_type_vector, begin_grammar_rule + offset))
+    {
+      grammar_rule_vector[cumulative_begin + offset] = text[begin_grammar_rule + offset];
+      ++offset;
+    }
+    grammar_rule_trie_node = insert_grammar_rule
+    (
+      grammar_rule_trie_root,
+      grammar_rule_vector,
+      cumulative_begin,
+      (cumulative_begin + offset),
+      (rank + 1)
+    );
+    reverse_grammar_rule_trie_root = insert_reverse_grammar_rule
+    (
+      reverse_grammar_rule_trie_root,
+      grammar_rule_vector,
+      cumulative_begin,
+      (cumulative_begin + offset),
+      (rank + 1)
+    )
+    cumulative_begin += offset;
+  }
 
   return 0;
 }
