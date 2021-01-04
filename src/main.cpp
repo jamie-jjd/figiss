10fb762e69887b16f51f9c4ce9fd48256cc349eb
jamie <jinjiedeng.jjd@gmail.com>
2021-01-04 15:57:10 +0800

    driver for benchmarking

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
+#include <fstream>
+#include <iostream>
 #include <stdexcept>
 
+#include <sdsl/suffix_trees.hpp>
+
+#include "benchmark.hpp"
 #include "grammar_compressed_index.hpp"
+#include "utility.hpp"
 
 int main (int argc, char **argv)
 {
   if (argc != 2)
   {
-    throw std::runtime_error(std::string("usage: ") + argv[0] + " [input path]");
+    throw std::runtime_error
+    (
+      std::string("usage: ")
+      + argv[0]
+      + " [text filename]"
+    );
   }
+  gci::gc_index index;
+  std::string index_path {"../input/index/gc_index_" + gci::util::basename(argv[1])};
+  gci::construct(index, argv[1]);
 
-  sdsl::int_vector<8> text;
-  sdsl::load_vector_from_file(text, argv[1], 1);
-  sdsl::append_zero_symbol(text);
+  gci::load(index, index_path);
+  sdsl::csa_wt<> fm_index;
+  std::ifstream fm_index_input {"../input/index/fm_index_" + gci::util::basename(argv[1])};
+  sdsl::load(fm_index, fm_index_input);
 
-  gc_index gci(text);
+  gci::benchmark_count(index, fm_index, argv[1], 1000, 20000);
 
   return 0;
 }
