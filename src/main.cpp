108099cdfbb65616346846952755dc39194c8639
jamie <jinjiedeng.jjd@gmail.com>
2021-01-04 16:35:30 +0800

    modified driver

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,15 @@ int main (int argc, char **argv)
   {
     throw std::runtime_error(std::string("usage: ") + argv[0] + " [input path]");
   }
+  gci::gc_index index;
+  std::string index_path {"../input/index/_gc_index_" + gci::util::basename(argv[1])};
+  gci::construct(index, argv[1]);
 
-<<<<<<< HEAD
   sdsl::csa_wt<> fm_index;
   std::ifstream fm_index_input {"../input/index/fm_index_" + gci::util::basename(argv[1])};
   sdsl::load(fm_index, fm_index_input);
-=======
-  sdsl::int_vector<8> text;
-  sdsl::load_vector_from_file(text, argv[1], 1);
-  sdsl::append_zero_symbol(text);
->>>>>>> parent of 10fb762... driver for benchmarking
-
-  gc_index gci(text);
+  
+  // gci::benchmark_count(index, fm_index, argv[1], 1000, 20000);
 
   return 0;
 }
