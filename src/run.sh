9f60bb038411e25ddb914f1564d3c39dc80b380a
jamie <jinjiedeng.jjd@gmail.com>
2021-01-04 15:56:48 +0800

    script for running benchmark

--- src/run.sh
+++ src/run.sh
@@ -2,21 +2,8 @@
 
 set -x
 
-make
-
-rm -f ../output/sample.csv
-./main ../input/text/sample.txt ../output/sample.csv
-
-# input_root_directory="../input/text/pizza_chili_repetitive_corpus/raw_corpus"
-# output_root_directory="../output/csv"
-#
-# for directory in `ls $input_root_directory`
-# do
-#   for file in `ls "$input_root_directory/$directory"`
-#   do
-#     input_file="$input_root_directory/$directory/$file"
-#     output_file="$output_root_directory/$file.csv"
-#     rm -f $output_file
-#     ./main $input_file $output_file
-#   done
-# done
+text_dir="/home/jamie/nthu_cs/research/projects/gci/project/input/text/pizza_chili_repetitive_corpus/1mb/*"
+for text in ${text_dir}
+do
+  ./main ${text}
+done
