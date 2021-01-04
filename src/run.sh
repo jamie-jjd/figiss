9707db6dd44806401f4c641609a6599b592d60e9
jamie <jinjiedeng.jjd@gmail.com>
2021-01-04 18:53:31 +0800

    script for running benchmark

--- src/run.sh
+++ src/run.sh
@@ -2,8 +2,12 @@
 
 set -x
 
+rm -f ../output/*/*
+
 text_dir="/home/jamie/nthu_cs/research/projects/gci/project/input/text/pizza_chili_repetitive_corpus/1mb/*"
 for text in ${text_dir}
 do
   ./main ${text}
 done
+
+rm -f ../input/pattern/*
