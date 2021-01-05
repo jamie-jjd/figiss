235f7c78f43a1ce1e0a7e46fee36acfad73b4447
jamie <jinjiedeng.jjd@gmail.com>
2021-01-05 21:06:59 +0800

    udpate script

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-text_dir="/home/jamie/nthu_cs/research/projects/gci/project/input/text/benchmark_corpus/*"
+text_dir="/home/jamie/nthu_cs/research/projects/gci/project/input/text/pizza_chili_repetitive_corpus/raw/*/*"
 for text in ${text_dir}
 do
   ./main ${text}
