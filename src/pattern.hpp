8ff2848f7924a4feea0857de2576be4f97906e29
jamie <jinjiedeng.jjd@gmail.com>
2021-01-23 20:01:12 +0800

    rewrite for restriction of pattern size

--- src/pattern.hpp
+++ src/pattern.hpp
@@ -17,28 +17,36 @@ void generate_pattern
 {
   sdsl::int_vector<8> text;
   sdsl::load_vector_from_file(text, text_path);
-  if (pattern_size > std::size(text))
+  if (pattern_size < std::size(text))
   {
-    throw std::runtime_error("pattern size is larger than text size");
-  }
-  std::ofstream pattern_output {pattern_path};
-  std::mt19937 engine {std::random_device{}()};
-  std::uniform_int_distribution<uint64_t> distribution(0, std::size(text) - pattern_size);
-  auto random_begin_dist {std::bind(distribution, engine)};
-  sdsl::int_vector<8> pattern(pattern_size);
-  pattern_output.write((char*)(&pattern_number), sizeof(pattern_number));
-  for (uint64_t i {0}; i != pattern_number; ++i)
-  {
-    auto text_it {std::next(std::begin(text), random_begin_dist())};
-    auto pattern_it {std::begin(pattern)};
-    auto pattern_end {std::end(pattern)};
-    while (pattern_it != pattern_end)
+    std::ofstream pattern_output {pattern_path};
+    std::mt19937 engine {std::random_device{}()};
+    std::uniform_int_distribution<uint64_t> distribution(0, std::size(text) - pattern_size);
+    auto random_begin_dist {std::bind(distribution, engine)};
+    sdsl::int_vector<8> pattern(pattern_size);
+    pattern_output.write((char*)(&pattern_number), sizeof(pattern_number));
+    for (uint64_t i {0}; i != pattern_number; ++i)
     {
-      *pattern_it = *text_it;
-      ++text_it;
-      ++pattern_it;
+      auto text_it {std::next(std::begin(text), random_begin_dist())};
+      auto pattern_it {std::begin(pattern)};
+      auto pattern_end {std::end(pattern)};
+      while (pattern_it != pattern_end)
+      {
+        *pattern_it = *text_it;
+        ++text_it;
+        ++pattern_it;
+      }
+      pattern.serialize(pattern_output);
     }
-    pattern.serialize(pattern_output);
+  }
+  else
+  {
+    std::cout << "pattern size >= text size\n";
+    std::cout << "generated pattern is one copy of the text\n";
+    std::ofstream pattern_output {pattern_path};
+    uint64_t pattern_number_ {1};
+    pattern_output.write((char*)(&pattern_number_), sizeof(pattern_number_));
+    text.serialize(pattern_output);
   }
   return;
 }
