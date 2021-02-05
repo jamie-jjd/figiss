9f6766cedbdf5f14628f022ba573cea8abc05423
jamie <jinjiedeng.jjd@gmail.com>
2021-02-05 16:02:47 +0800

    modify timer-related part

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -16,7 +16,7 @@ void load_gc_and_fm_index
   std::string const text_path
 )
 {
-  std::string gc_index_path {"../input/index/" + util::basename(text_path) + ".gci"};
+  std::string gc_index_path {"../input/index/" + gci::basename(text_path) + ".gci"};
   std::ifstream gc_index_input {gc_index_path};
   if (!gc_index_input.is_open())
   {
@@ -27,13 +27,13 @@ void load_gc_and_fm_index
   {
     load(index, gc_index_path);
   }
-  std::string fm_index_path {"../input/index/" + util::basename(text_path) + ".fmi"};
+  std::string fm_index_path {"../input/index/" + gci::basename(text_path) + ".fmi"};
   std::ifstream fm_index_input {fm_index_path};
   if (!fm_index_input.is_open())
   {
     {
 #ifdef LOG_FMI_CONSTRUCT
-      std::ofstream json_output {"../output/construct/" + util::basename(text_path) + ".fmi.json"};
+      std::ofstream json_output {"../output/construct/" + gci::basename(text_path) + ".fmi.json"};
       tdc::StatPhase phases {"construct_fm_index"};
 #endif
       sdsl::int_vector<8> text;
@@ -55,7 +55,7 @@ void load_gc_and_fm_index
     {
       std::ofstream fm_index_output {fm_index_path};
 #ifdef LOG_FMI_SERIALIZE
-      std::ofstream json_output {"../output/serialize/" + util::basename(text_path) + ".fmi.json"};
+      std::ofstream json_output {"../output/serialize/" + gci::basename(text_path) + ".fmi.json"};
       tdc::StatPhase phases {"serialize_fm_index"};
       tdc::StatPhase::wrap
       (
@@ -74,7 +74,7 @@ void load_gc_and_fm_index
   else
   {
 #ifdef LOG_FMI_LOAD
-    std::ofstream json_output {"../output/load/" + util::basename(text_path) + ".fmi.json"};
+    std::ofstream json_output {"../output/load/" + gci::basename(text_path) + ".fmi.json"};
     tdc::StatPhase phases {"load_fm_index"};
     tdc::StatPhase::wrap
     (
@@ -103,13 +103,13 @@ void benchmark_gc_index_count
   uint64_t pattern_number {0};
   pattern_input.read((char*)(&pattern_number), sizeof(pattern_number));
 #if defined(LOG_GCI_COUNT) || defined(LOG_VERBOSE_GCI_COUNT)
-  gci::util::timer::register_category(gci::util::category_id::GCI_COUNT, "gc_index_count");
+  gci::timer::register_record(gci::record_key::GCI_COUNT, "gc_index_count");
 #endif
 #if defined(LOG_VERBOSE_GCI_COUNT)
-  gci::util::timer::register_category(gci::util::category_id::CALCULATE_SL_FACTOR, "calculate_sl_factor");
-  gci::util::timer::register_category(gci::util::category_id::WAVELET_TREE_OPERATIONS_L, "wavelet_tree_operations_L");
-  gci::util::timer::register_category(gci::util::category_id::LOOKUP_GRAMMAR_RULE, "lookup_grammar_rule");
-  gci::util::timer::register_category(gci::util::category_id::WAVELET_TREE_OPERATIONS_S, "wavelet_tree_operations_S");
+  gci::timer::register_record(gci::record_key::CALCULATE_SL_FACTOR, "calculate_sl_factor");
+  gci::timer::register_record(gci::record_key::WAVELET_TREE_OPERATIONS_L, "wavelet_tree_operations_L");
+  gci::timer::register_record(gci::record_key::LOOKUP_GRAMMAR_RULE, "lookup_grammar_rule");
+  gci::timer::register_record(gci::record_key::WAVELET_TREE_OPERATIONS_S, "wavelet_tree_operations_S");
 #endif
   for (uint64_t i {0}; i != pattern_number; ++i)
   {
@@ -117,15 +117,15 @@ void benchmark_gc_index_count
     gci::count(index, std::begin(pattern), std::end(pattern));
   }
 #if defined(LOG_GCI_COUNT)
-  gci::util::timer::print
+  gci::timer::print
   (
-    "../output/count/" + util::basename(pattern_path) + ".gci.csv",
+    "../output/count/" + gci::basename(pattern_path) + ".gci.csv",
     "milliseconds"
   );
 #elif defined(LOG_VERBOSE_GCI_COUNT)
-  gci::util::timer::print
+  gci::timer::print
   (
-    "../output/count/" + util::basename(pattern_path) + ".gci.verbose.csv",
+    "../output/count/" + gci::basename(pattern_path) + ".gci.verbose.csv",
     "milliseconds"
   );
 #endif
@@ -143,23 +143,23 @@ void benchmark_fm_index_count
   uint64_t pattern_number {0};
   pattern_input.read((char*)(&pattern_number), sizeof(pattern_number));
 #ifdef LOG_FMI_COUNT
-  gci::util::timer::register_category(gci::util::category_id::FMI_COUNT, "fm_index_count");
+  gci::timer::register_record(gci::record_key::FMI_COUNT, "fm_index_count");
 #endif
   for (uint64_t i {0}; i != pattern_number; ++i)
   {
     pattern.load(pattern_input);
 #ifdef LOG_FMI_COUNT
-    gci::util::timer::resume(gci::util::category_id::FMI_COUNT);
+    gci::timer::resume(gci::record_key::FMI_COUNT);
 #endif
     sdsl::count(fm_index, std::begin(pattern), std::end(pattern));
 #ifdef LOG_FMI_COUNT
-  gci::util::timer::pause(gci::util::category_id::FMI_COUNT);
+  gci::timer::pause(gci::record_key::FMI_COUNT);
 #endif
   }
 #ifdef LOG_FMI_COUNT
-  gci::util::timer::print
+  gci::timer::print
   (
-    "../output/count/" + util::basename(pattern_path) + ".fmi.csv",
+    "../output/count/" + gci::basename(pattern_path) + ".fmi.csv",
     "milliseconds"
   );
 #endif
@@ -186,11 +186,11 @@ void benchmark_count
   std::string pattern_path
   {
     "../input/pattern/"
-    + util::basename(text_path) + "."
+    + gci::basename(text_path) + "."
     + std::to_string(pattern_number) + "."
     + std::to_string(pattern_size)
   };
-  gci::util::generate_pattern
+  gci::generate_pattern
   (
     text_path,
     pattern_path,
@@ -218,7 +218,7 @@ void test_count (std::string const text_path)
   for (uint64_t multiple {2}; multiple != 11; ++multiple)
   {
     uint64_t pattern_number {1000 / multiple};
-    gci::util::generate_pattern
+    gci::generate_pattern
     (
       text_path,
       pattern_path,
@@ -250,7 +250,7 @@ void test_count (std::string const text_path)
     {
       // uint64_t pattern_number {10 * divisor};
       uint64_t pattern_number {1};
-      gci::util::generate_pattern
+      gci::generate_pattern
       (
         text_path,
         pattern_path,
