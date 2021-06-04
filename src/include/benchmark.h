0f6550b1d9209f86823d72f2b7fabbf04a1b0d79
jamie <jinjiedeng.jjd@gmail.com>
2021-06-04 11:53:44 +0800

    rewrite testing

--- src/include/benchmark.h
+++ src/include/benchmark.h
@@ -8,46 +8,6 @@
 
 namespace project
 {
-template <typename Fmindex>
-void LoadIndexAndFmindex
-(
-  Index &index,
-  Fmindex &fm_index,
-  std::filesystem::path const &text_path
-)
-{
-  auto parent_index_path {CreateParentDirectoryByCategory("index", text_path)};
-  auto index_path {CreatePath(parent_index_path, text_path.filename().string(), ".index")};
-  // if (!std::filesystem::exists(index_path))
-  // {
-    std::cout << "construct & serialize index ...\n";
-    ConstructIndex(index, text_path);
-    SerializeIndex(index, index_path);
-  // }
-  // else
-  // {
-  //   std::cout << "load index ...\n";
-  //   LoadIndex(index, index_path);
-  // }
-  auto fm_index_path {CreatePath(parent_index_path, text_path.filename().string(), ".fm_index")};
-  if (!std::filesystem::exists(fm_index_path))
-  {
-    sdsl::int_vector<8> text;
-    sdsl::load_vector_from_file(text, text_path);
-    std::cout << "construct & serialize FM-index ...\n";
-    sdsl::construct_im(fm_index, text);
-    std::fstream fm_index_file(fm_index_path, std::ios_base::out | std::ios_base::trunc);
-    sdsl::serialize(fm_index, fm_index_file);
-  }
-  else
-  {
-    std::ifstream fm_index_file {fm_index_path};
-    std::cout << "load FM-index ...\n";
-    sdsl::load(fm_index, fm_index_file);
-  }
-  return;
-}
-
 // template
 // <
 //   typename PatternAmount,
@@ -82,7 +42,7 @@ void LoadIndexAndFmindex
 // >
 // void BenchmarkFmindexCount
 // (
-//   Fmindex const &fm_index,
+//   Fmindex const &rlfm,
 //   PatternAmount const pattern_amount,
 //   PatternSize const unit_size,
 //   Patterns &patterns
@@ -94,7 +54,7 @@ void LoadIndexAndFmindex
 //   {
 //     begin = end;
 //     end = std::next(begin, unit_size);
-//     sdsl::count(fm_index, begin, end);
+//     sdsl::count(rlfm, begin, end);
 //   }
 //   return;
 // }
@@ -116,62 +76,87 @@ void LoadIndexAndFmindex
 //     );
 //   }
 //   Index index;
-//   sdsl::csa_wt<> fm_index;
-//   LoadIndexAndFmindex(index, fm_index, text_path);
+//   sdsl::csa_wt<> rlfm;
+//   LoadIndexAndFmindex(index, rlfm, text_path);
 //   sdsl::int_vector<8> patterns;
 //   GeneratePatterns(text_path, pattern_amount, unit_size, patterns);
 //   BenchmarkIndexCount(index, pattern_amount, unit_size, patterns);
-//   BenchmarkFmindexCount(fm_index, pattern_amount, unit_size, patterns);
+//   BenchmarkFmindexCount(rlfm, pattern_amount, unit_size, patterns);
 //   return;
 // }
 
 void TestCount (std::filesystem::path const &text_path)
 {
+  auto parent_index_path {CreateParentDirectoryByCategory("index", text_path)};
   Index index;
-  sdsl::csa_wt<> fm_index;
-  LoadIndexAndFmindex(index, fm_index, text_path);
-  sdsl::int_vector<8> text;
-  sdsl::load_vector_from_file(text, text_path);
-  for (uint64_t unit_size {1}; (unit_size <= 100) && (unit_size < std::size(text) + 1); ++unit_size)
   {
-    auto patterns {ConcatenatedPatterns{100, unit_size}};
-    GenerateConcatnatedPatterns(text_path, patterns);
-    auto begin {std::begin(patterns.labels)};
-    auto end {begin};
-    for (uint64_t i {}; i != patterns.amount; ++i)
+    auto index_path {CreatePath(parent_index_path, text_path.filename().string(), ".index")};
+    // if (!std::filesystem::exists(index_path))
     {
-      begin = end;
-      end = std::next(begin, patterns.unit_size);
-      auto fm_index_count {sdsl::count(fm_index, begin, end)};
-      auto index_count {Count(index, begin, end)};
-      if (fm_index_count != index_count)
-      {
-        throw std::runtime_error
-        (
-          "failed at size: " +
-          std::to_string(unit_size)
-        );
-      }
+      ConstructIndex(index, text_path);
+      SerializeIndex(index, index_path);
     }
-    std::cout << "succeed at size: " << std::to_string(unit_size) << "\n";
+    // else
+    // {
+    //   LoadIndex(index, index_path);
+    // }
   }
-  for (uint64_t unit_size {std::size(text)}; unit_size > 100; unit_size /= 2)
+  sdsl::csa_wt
+  <
+    sdsl::wt_rlmn<>,
+    std::numeric_limits<uint32_t>::max(),
+    std::numeric_limits<uint32_t>::max()
+  >
+  rlfm;
   {
-    auto patterns {ConcatenatedPatterns{1, unit_size}};
-    GenerateConcatnatedPatterns(text_path, patterns);
-    auto begin {std::begin(patterns.labels)};
-    auto end {std::end(patterns.labels)};
-    auto fm_index_count {sdsl::count(fm_index, begin, end)};
-    auto index_count {Count(index, begin, end)};
-    if (fm_index_count != index_count)
+    auto rlfm_path {CreatePath(parent_index_path, text_path.filename().string(), ".rlfm")};
+    // if (!std::filesystem::exists(rlfm_path))
     {
-      throw std::runtime_error
-      (
-        "failed at size: " +
-        std::to_string(unit_size)
-      );
+      sdsl::int_vector<8> text;
+      sdsl::load_vector_from_file(text, text_path);
+      std::cout << "construct rlfm of " << std::filesystem::canonical(text_path) << "\n";
+      sdsl::construct_im(rlfm, text);
+      std::fstream rlfm_file(rlfm_path, std::ios_base::out | std::ios_base::trunc);
+      std::cout << "serialize rlfm to " << std::filesystem::canonical(rlfm_path) << "\n";
+      sdsl::serialize(rlfm, rlfm_file);
     }
-    std::cout << "succeed at size: " + std::to_string(unit_size) << "\n";
+    // else
+    // {
+    //   std::ifstream rlfm_file {rlfm_path};
+    //   std::cout << "load rlfm from " << std::filesystem::canonical(rlfm_path) << "\n";
+    //   sdsl::load(rlfm, rlfm_file);
+    // }
+  }
+  {
+    sdsl::int_vector<8> text;
+    sdsl::load_vector_from_file(text, text_path);
+    uint64_t max_unit_size {std::min(std::size(text), (1UL << 20))};
+    std::cout << "test pattern counting\namount\tsize\n";
+    for (uint64_t unit_size {1}; unit_size <= max_unit_size; unit_size *= 2)
+    {
+      auto patterns {ConcatenatedPatterns{std::min(max_unit_size / unit_size, (1UL << 13)), unit_size}};
+      GenerateConcatnatedPatterns(text_path, patterns);
+      auto begin {std::begin(patterns.labels)};
+      auto end {begin};
+      for (uint64_t i {}; i != patterns.amount; ++i)
+      {
+        begin = end;
+        end = std::next(begin, patterns.unit_size);
+        auto rlfm_count {sdsl::count(rlfm, begin, end)};
+        auto index_count {Count(index, begin, end)};
+        if (rlfm_count != index_count)
+        {
+          std::string pattern(begin, end);
+          throw std::runtime_error
+          (
+            "\033[31mfailed at pattern: " + pattern
+            + ", real count: " + std::to_string(rlfm_count)
+            + ", count: " + std::to_string(index_count)
+            + "\033[0m");
+        }
+      }
+      std::cout << std::to_string(patterns.amount) << "\t" << std::to_string(unit_size) << "\t\033[32msucceed\033[0m\n";
+    }
   }
   return;
 }
