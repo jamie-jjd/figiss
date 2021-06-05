008d793d3d43b47209cd1077915e8a081ff24070
jamie <jinjiedeng.jjd@gmail.com>
2021-06-05 17:33:40 +0800

    update script

--- src/run.sh
+++ src/run.sh
@@ -2,24 +2,11 @@
 
 set -x
 
-prefix="/home/jamie/nthu_cs/research/projects/gci/project/data"
+files="../data/sample.txt"
+# files="../data/corpus/*"
 
-# category_prefix="statistics/run_length_wavelet_tree"
-
-category="corpus"
-# category="index"
-
-size="1mb/*"
-# size="40mb/*"
-# size="raw/*"
-
-dir=${prefix}/${category}/${size}
-
-for entry in ${dir}
+for file in ${files}
 do
-  # echo ${entry}
-  ./main ${entry}
+  # echo ${file}
+  ./main ${file}
 done
-
-# ./main ../data/sample.txt
-# ./main ${prefix}/${category}/1mb/kernel.1mb
