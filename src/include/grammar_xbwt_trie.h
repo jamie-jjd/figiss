504c9c522d34547dba82b6eb9b7416dacca35169
jamie <jinjiedeng.jjd@gmail.com>
2021-09-06 15:24:24 +0800

    replace rank_range with rank finish constructor

--- src/include/grammar_xbwt_trie.h
+++ src/include/grammar_xbwt_trie.h
@@ -6,7 +6,7 @@
 
 #include <sdsl/wavelet_trees.hpp>
 
-#include "symbol_bucket_offsets.h"
+#include "sparse_prefix_sum.h"
 
 namespace figiss
 {
@@ -18,10 +18,16 @@ public:
   {
     uint64_t label;
     uint64_t count;
-    std::pair<uint64_t, uint64_t> rank_range;
+    uint64_t rank;
     std::map<uint64_t, std::shared_ptr<Node>> children;
 
-    Node (uint64_t const label = 0): label {label} {}
+    Node (uint64_t const label = 0)
+    : label {label},
+      count {},
+      rank {},
+      children {}
+    {
+    }
   };
 
   GrammarTrie (uint8_t const effective_alphabet_width, uint64_t const length_width)
@@ -33,7 +39,7 @@ public:
 
   template <typename Iterator>
   void Insert (Iterator it, Iterator end);
-  void CalculateCumulativeCountAndRankRange ();
+  void CalculateCumulativeCountAndRank ();
 
   inline auto GetRoot () const
   {
@@ -74,11 +80,11 @@ void GrammarTrie::Insert (Iterator it, Iterator end)
     ++it;
   }
   ++(node->count);
-  std::get<0>(node->rank_range) = 1;
+  node->rank = 1;
   return;
 }
 
-void GrammarTrie::CalculateCumulativeCountAndRankRange ()
+void GrammarTrie::CalculateCumulativeCountAndRank ()
 {
   uint64_t rank {};
   std::deque<std::pair<std::shared_ptr<GrammarTrie::Node>, bool>> nodes;
@@ -90,9 +96,9 @@ void GrammarTrie::CalculateCumulativeCountAndRankRange ()
     if (is_forward)
     {
       is_forward = false;
-      if (std::get<0>(node->rank_range))
+      if (node->rank)
       {
-        std::get<0>(node->rank_range) = std::get<1>(node->rank_range) = ++rank;
+        node->rank = ++rank;
       }
       for (auto it {std::rbegin(node->children)}; it != std::rend(node->children); ++it)
       {
@@ -101,19 +107,9 @@ void GrammarTrie::CalculateCumulativeCountAndRankRange ()
     }
     else
     {
-      if (!node->children.empty())
+      for (auto const& pair : node->children)
       {
-        for (auto const& pair : node->children)
-        {
-          node->count += std::get<1>(pair)->count;
-        }
-        auto first_child {std::get<1>(*std::begin(node->children))};
-        auto last_child {std::get<1>(*std::rbegin(node->children))};
-        if (std::get<0>(node->rank_range) == 0)
-        {
-          std::get<0>(node->rank_range) = std::get<0>(first_child->rank_range);
-        }
-        std::get<1>(node->rank_range) = std::get<1>(last_child->rank_range);
+        node->count += std::get<1>(pair)->count;
       }
       nodes.pop_back();
     }
@@ -123,7 +119,7 @@ void GrammarTrie::CalculateCumulativeCountAndRankRange ()
 
 std::ostream& operator<< (std::ostream& out, GrammarTrie const& grammar_trie)
 {
-  out << "depth:(byte_rank,length)(count)[leftmost_rank:rightmost_rank]\n";
+  out << "depth:(byte_rank,length)(count)(rank)\n";
   std::deque<std::pair<std::shared_ptr<GrammarTrie::Node>, uint64_t>> nodes;
   nodes.emplace_back(grammar_trie.root_, 0);
   uint64_t size {};
@@ -139,7 +135,7 @@ std::ostream& operator<< (std::ostream& out, GrammarTrie const& grammar_trie)
       out << depth << ":"
       << "(" << (node->label / divisor) << "," << (node->label % divisor) << ")"
       << "(" << node->count << ")"
-      << "[" << std::get<0>(node->rank_range) << "," << std::get<1>(node->rank_range) << "]\n";
+      << "(" << node->rank << ")\n";
     }
     for (auto it {std::rbegin(node->children)}; it != std::rend(node->children); ++it)
     {
@@ -198,10 +194,10 @@ private:
     typename sdsl::sd_vector<>::select_1_type,
     sdsl::wt_ap<>
   >
-  bwt_;
-  SymbolBucketOffsets symbol_bucket_offsets_;
-  sdsl::int_vector<> counts_;
-  sdsl::int_vector<> lex_rank_range_;
+  child_labels_;
+  SparsePrefixSum label_bucket_offsets_;
+  SparsePrefixSum counts_;
+  sdsl::int_vector<> colex_to_lex_rank_;
 
 };
 
@@ -218,18 +214,18 @@ GrammarXbwtTrie::GrammarXbwtTrie (GrammarTrie const& grammar_trie)
   );
   // {
   //   uint64_t divisor {1ULL << grammar_trie.GetLengthWidth()};
-  //   for (auto const& symbol : reverse_grammar_rules)
+  //   for (auto const& label : reverse_grammar_rules)
   //   {
-  //     std::cout << "(" << (symbol / divisor) << "," << (symbol % divisor) << ")";
+  //     std::cout << "(" << (label / divisor) << "," << (label % divisor) << ")";
   //   }
   //   std::cout << "\n";
-  //   std::cout << "(byte_rank,length)(count)[leftmost_rank:rightmost_rank]\n";
+  //   std::cout << "(byte_rank,length)(count)(rank)\n";
   //   for (auto node : grammar_trie_nodes)
   //   {
   //     std::cout
   //     << "(" << (node->label / divisor) << "," << (node->label % divisor) << ")"
   //     << "(" << node->count << ")"
-  //     << "[" << std::get<0>(node->rank_range) << "," << std::get<1>(node->rank_range) << "]\n";
+  //     << "(" << node->rank << ")\n";
   //   }
   // }
   sdsl::int_vector<> suffix_array;
@@ -237,21 +233,142 @@ GrammarXbwtTrie::GrammarXbwtTrie (GrammarTrie const& grammar_trie)
   sdsl::int_vector<> reduced_commom_prefix_lengths;
   CalculateReducedCommomPrefixLengths(reverse_grammar_rules, suffix_array, reduced_commom_prefix_lengths);
   // {
-  //   std::cout << "i\tRCP\tSA\tT[i:]\n";
+  //   uint64_t divisor {1ULL << grammar_trie.GetLengthWidth()};
+  //   std::cout << "i\tRCP\tBWT\tSA\treverse_grammar_rules[i:]\n";
   //   for (uint64_t i {}; i != std::size(suffix_array); ++i)
   //   {
   //     auto it {std::next(std::begin(reverse_grammar_rules), suffix_array[i])};
-  //     std::cout
-  //     << i << "\t"
-  //     << reduced_commom_prefix_lengths[i] << "\t"
-  //     << suffix_array[i] << "\t";
+  //     std::cout << i << "\t";
+  //     std::cout << reduced_commom_prefix_lengths[i] << "\t";
+  //     if (suffix_array[i])
+  //     {
+  //       auto label {reverse_grammar_rules[suffix_array[i] - 1]};
+  //       std::cout << "(" << (label / divisor) << "," << (label % divisor) << ")\t";
+  //     }
+  //     else
+  //     {
+  //       std::cout
+  //       << "(" << (GrammarXbwtTrie::kSpecialSymbol / divisor)
+  //       << "," << (GrammarXbwtTrie::kSpecialSymbol % divisor)
+  //       << ")\t";
+  //     }
+  //     std::cout << suffix_array[i] << "\t";
   //     while (it != std::end(reverse_grammar_rules))
   //     {
-  //       std::cout << *it++ << " ";
+  //       std::cout << "(" << (*it / divisor) << "," << (*it % divisor) << ")";
+  //       ++it;
   //     }
   //     std::cout << "\n";
   //   }
   // }
+  {
+    std::deque<uint8_t> trie_bits;
+    std::set<uint64_t> label_set;
+    std::deque<uint64_t> child_labels;
+    std::deque<uint64_t> label_bucket_offsets;
+    std::deque<uint64_t> counts;
+    std::deque<uint64_t> colex_to_lex_rank;
+    {
+      label_bucket_offsets.push_back(0);
+      label_bucket_offsets.push_back(0);
+      counts.push_back(0);
+    }
+    for (uint64_t i {1}, j {suffix_array[i]}; i != std::size(suffix_array); ++i, j = suffix_array[i])
+    {
+      uint64_t label {};
+      label = ((j) ? reverse_grammar_rules[j - 1] : GrammarXbwtTrie::kSpecialSymbol);
+      if (label == GrammarXbwtTrie::kSpecialSymbol)
+      {
+        colex_to_lex_rank.push_back(grammar_trie_nodes[j]->rank);
+      }
+      if (reverse_grammar_rules[j + reduced_commom_prefix_lengths[i]] == GrammarXbwtTrie::kSpecialSymbol)
+      {
+        if (label_set.count(label) == 0 || label == GrammarXbwtTrie::kSpecialSymbol)
+        {
+          trie_bits.push_back(0);
+          ++label_bucket_offsets.back();
+        }
+      }
+      else
+      {
+        if (!trie_bits.empty() && trie_bits.back() == 0)
+        {
+          trie_bits.back() = 1;
+          trie_bits.push_back(0);
+        }
+        else
+        {
+          trie_bits.push_back(1);
+        }
+        for (auto const label : label_set)
+        {
+          child_labels.push_back(label);
+        }
+        label_set.clear();
+        if (reverse_grammar_rules[j] != reverse_grammar_rules[suffix_array[i - 1]])
+        {
+          label_bucket_offsets.push_back(1);
+        }
+        else
+        {
+          ++label_bucket_offsets.back();
+        }
+        counts.push_back(grammar_trie_nodes[j]->count);
+      }
+      label_set.insert(label);
+    }
+    {
+      trie_bits.back() = 1;
+      trie_bits_.resize(std::size(trie_bits));
+      std::copy(std::begin(trie_bits), std::end(trie_bits), std::begin(trie_bits_));
+      trie_rank_1_ = decltype(trie_rank_1_)(&trie_bits_);
+      trie_select_1_ = decltype(trie_select_1_)(&trie_bits_);
+      // {
+      //   std::cout << "trie_bits: ";
+      //   Print(trie_bits, std::cout);
+      // }
+    }
+    {
+      child_labels.push_back(reverse_grammar_rules[*std::prev(std::end(suffix_array)) - 1]);
+      sdsl::int_vector<> temp_child_labels(std::size(child_labels));
+      std::copy(std::begin(child_labels), std::end(child_labels), std::begin(temp_child_labels));
+      sdsl::construct_im(child_labels_, temp_child_labels);
+      // {
+      //   uint64_t divisor {1ULL << grammar_trie.GetLengthWidth()};
+      //   std::cout << "child_labels: ";
+      //   for (auto const &label : child_labels)
+      //   {
+      //     std::cout << "(" << (label / divisor) << "," << (label % divisor) << ")";
+      //   }
+      //   std::cout << "\n";
+      // }
+    }
+    {
+      std::partial_sum(std::begin(label_bucket_offsets), std::end(label_bucket_offsets), std::begin(label_bucket_offsets));
+      label_bucket_offsets_ = decltype(label_bucket_offsets_)(label_bucket_offsets);
+      // {
+      //   std::cout << "label_bucket_offsets: ";
+      //   Print(label_bucket_offsets, std::cout);
+      // }
+    }
+    {
+      std::partial_sum(std::begin(counts), std::end(counts), std::begin(counts));
+      counts_ = decltype(counts_)(counts);
+      // {
+      //   std::cout << "counts: ";
+      //   Print(counts, std::cout);
+      // }
+    }
+    {
+      colex_to_lex_rank_.width(sdsl::bits::hi(std::size(colex_to_lex_rank)) + 1);
+      colex_to_lex_rank_.resize(std::size(colex_to_lex_rank));
+      std::copy(std::begin(colex_to_lex_rank), std::end(colex_to_lex_rank), std::begin(colex_to_lex_rank_));
+      // {
+      //   std::cout << "colex_to_lex_rank: ";
+      //   Print(colex_to_lex_rank, std::cout);
+      // }
+    }
+  }
 }
 
 GrammarXbwtTrie& GrammarXbwtTrie::operator= (GrammarXbwtTrie&& grammar_xbwt_trie)
@@ -263,10 +380,10 @@ GrammarXbwtTrie& GrammarXbwtTrie::operator= (GrammarXbwtTrie&& grammar_xbwt_trie
     trie_rank_1_.set_vector(&trie_bits_);
     trie_select_1_ = std::move(grammar_xbwt_trie.trie_select_1_);
     trie_select_1_.set_vector(&trie_bits_);
-    bwt_ = std::move(grammar_xbwt_trie.bwt_);
-    symbol_bucket_offsets_ = std::move(grammar_xbwt_trie.symbol_bucket_offsets_);
+    child_labels_ = std::move(grammar_xbwt_trie.child_labels_);
+    label_bucket_offsets_ = std::move(grammar_xbwt_trie.label_bucket_offsets_);
     counts_ = std::move(grammar_xbwt_trie.counts_);
-    lex_rank_range_ = std::move(grammar_xbwt_trie.lex_rank_range_);
+    colex_to_lex_rank_ = std::move(grammar_xbwt_trie.colex_to_lex_rank_);
   }
   return *this;
 }
@@ -342,12 +459,7 @@ void GrammarXbwtTrie::CalculateReverseGrammarRulesAndGrammarTrieNodes
     {
       is_forward = false;
       ancestors.push_back(node);
-      if
-      (
-        node->children.empty()
-        ||
-        std::get<0>(node->rank_range) < std::get<0>(std::get<1>(*std::begin(node->children))->rank_range)
-      )
+      if (node->rank)
       {
         for (auto it {std::rbegin(ancestors)}; it != std::rend(ancestors); ++it)
         {
