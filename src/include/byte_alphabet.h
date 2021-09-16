e4e912bd3988f1d9e6a6c9ad303f5020bf7e18bc
jamie <jinjiedeng.jjd@gmail.com>
2021-09-16 09:36:30 +0800

    add GetAlphabetWidth() remove effective_alphabet_width_ update move semantics

--- src/include/byte_alphabet.h
+++ src/include/byte_alphabet.h
@@ -26,9 +26,19 @@ public:
   );
   void Load (std::istream& in);
 
+  inline uint8_t GetAlphabetWidth () const
+  {
+    return sdsl::bits::hi(std::size(effective_alphabet_bits_) - 1) + 1;
+  }
+
   inline uint8_t GetEffectiveAlphabetWidth () const
   {
-    return effective_alphabet_width_;
+    auto size {std::size(effective_alphabet_bits_)};
+    if (size)
+    {
+      return sdsl::bits::hi(effective_alphabet_rank_1_(std::size(effective_alphabet_bits_)) - 1) + 1;
+    }
+    return size;
   }
 
   inline uint64_t GetRank (uint64_t const byte) const
@@ -44,7 +54,6 @@ public:
 
 private:
 
-  uint8_t effective_alphabet_width_;
   sdsl::bit_vector effective_alphabet_bits_;
   sdsl::bit_vector::rank_1_type effective_alphabet_rank_1_;
 
@@ -64,17 +73,13 @@ ByteAlphabet::ByteAlphabet (sdsl::int_vector<8> const& byte_text)
     effective_alphabet_bits_[byte] = 1;
   }
   effective_alphabet_rank_1_ = decltype(effective_alphabet_rank_1_)(&effective_alphabet_bits_);
-  effective_alphabet_width_ = sdsl::bits::hi(std::size(effective_alphabet) - 1) + 1;
 }
 
 ByteAlphabet::ByteAlphabet (ByteAlphabet&& byte_alphabet)
 {
   if (this != &byte_alphabet)
   {
-    effective_alphabet_width_ = std::move(byte_alphabet.effective_alphabet_width_);
-    effective_alphabet_bits_ = std::move(byte_alphabet.effective_alphabet_bits_);
-    effective_alphabet_rank_1_ = std::move(byte_alphabet.effective_alphabet_rank_1_);
-    effective_alphabet_rank_1_.set_vector(&effective_alphabet_bits_);
+    this->Swap(byte_alphabet);
   }
 }
 
@@ -90,8 +95,8 @@ ByteAlphabet& ByteAlphabet::operator= (ByteAlphabet&& byte_alphabet)
 
 void ByteAlphabet::Swap (ByteAlphabet& byte_alphabet)
 {
-  std::swap(effective_alphabet_width_, byte_alphabet.effective_alphabet_width_);
   effective_alphabet_bits_.swap(byte_alphabet.effective_alphabet_bits_);
+  effective_alphabet_rank_1_.swap(byte_alphabet.effective_alphabet_rank_1_);
   effective_alphabet_rank_1_.set_vector(&effective_alphabet_bits_);
   byte_alphabet.effective_alphabet_rank_1_.set_vector(&byte_alphabet.effective_alphabet_bits_);
   return;
@@ -107,14 +112,12 @@ uint64_t ByteAlphabet::Serialize
   uint64_t size_in_bytes {};
   if (!parent)
   {
-    sdsl::write_member(effective_alphabet_width_, out);
     sdsl::serialize(effective_alphabet_bits_, out);
     sdsl::serialize(effective_alphabet_rank_1_, out);
   }
   else
   {
     auto node {std::make_shared<SpaceNode>(name)};
-    node->AddLeaf("effective_alphabet_width_", sdsl::write_member(effective_alphabet_width_, out));
     node->AddLeaf("effective_alphabet_bits_", sdsl::serialize(effective_alphabet_bits_, out));
     node->AddLeaf("effective_alphabet_rank_1_", sdsl::serialize(effective_alphabet_rank_1_, out));
     parent->AddChild(node);
@@ -125,7 +128,6 @@ uint64_t ByteAlphabet::Serialize
 
 void ByteAlphabet::Load (std::istream& in)
 {
-  sdsl::read_member(effective_alphabet_width_, in);
   effective_alphabet_bits_.load(in);
   effective_alphabet_rank_1_.load(in);
   effective_alphabet_rank_1_.set_vector(&effective_alphabet_bits_);
@@ -135,19 +137,20 @@ void ByteAlphabet::Load (std::istream& in)
 std::ostream& operator<< (std::ostream& out, ByteAlphabet const& byte_alphabet)
 {
   {
-    out << "effective_alphabet_width_:";
-    out << static_cast<uint64_t>(byte_alphabet.effective_alphabet_width_) << "\n";
+    out << "alphabet_width:";
+    out << static_cast<uint64_t>(byte_alphabet.GetAlphabetWidth()) << "\n";
+    out << "effective_alphabet_width:";
+    out << static_cast<uint64_t>(byte_alphabet.GetEffectiveAlphabetWidth()) << "\n";
     out << "byte_alphabet:\n";
     for (uint64_t byte {}; byte != std::size(byte_alphabet.effective_alphabet_bits_); ++byte)
     {
       if (byte_alphabet.effective_alphabet_bits_[byte])
       {
-        out << byte_alphabet.GetRank(byte) << ":" << byte  << "(" << static_cast<char>(byte) << ")" << "\n";
+        out << byte_alphabet.GetRank(byte) << ":" << byte << "(" << static_cast<char>(byte) << ")" << "\n";
       }
     }
   }
   {
-    out << "effective_alphabet_width_:" << ProperSizeRepresentation(sizeof(byte_alphabet.effective_alphabet_width_)) << "B\n";
     out << "effective_alphabet_bits_:" << ProperSizeRepresentation(sdsl::size_in_bytes(byte_alphabet.effective_alphabet_bits_)) << "B\n";
     out << "effective_alphabet_rank_1_:" << ProperSizeRepresentation(sdsl::size_in_bytes(byte_alphabet.effective_alphabet_rank_1_)) << "B\n";
   }
