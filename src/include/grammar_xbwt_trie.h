aa0b336bacd823e32ca4f9d2f9b5b6996716bc17
jamie <jinjiedeng.jjd@gmail.com>
2021-09-20 10:23:02 +0800

    remove run_length_alphabet_ update Insert() for cumulative count move CalculateCumulativeCountAndRank() to index.h

--- src/include/grammar_xbwt_trie.h
+++ src/include/grammar_xbwt_trie.h
@@ -4,11 +4,6 @@
 #include <map>
 #include <memory>
 
-#include <sdsl/wavelet_trees.hpp>
-
-#include "integer_alphabet.h"
-#include "sparse_prefix_sum.h"
-
 namespace figiss
 {
 class GrammarTrie
@@ -31,10 +26,9 @@ public:
     }
   };
 
-  GrammarTrie (ByteAlphabet const& byte_alphabet, IntegerAlphabet const& run_length_alphabet)
+  GrammarTrie (uint8_t const length_width)
   : root_ {std::make_shared<Node>()},
-    length_width_ {static_cast<uint8_t>(run_length_alphabet.GetAlphabetWidth() - byte_alphabet.GetEffectiveAlphabetWidth())},
-    run_length_alphabet_ {run_length_alphabet}
+    length_width_ {length_width}
   {
   }
 
@@ -47,28 +41,12 @@ public:
     return root_;
   }
 
-  inline uint8_t GetLengthWidth () const
-  {
-    return length_width_;
-  }
-
-  inline uint8_t GetAlphabetWidth () const
-  {
-    return run_length_alphabet_.GetAlphabetWidth();
-  }
-
-  inline uint8_t GetEffectiveAlphabetWidth () const
-  {
-    return run_length_alphabet_.GetEffectiveAlphabetWidth();
-  }
-
   friend std::ostream& operator<< (std::ostream& out, GrammarTrie const& grammar_trie);
 
 private:
 
   std::shared_ptr<Node> root_;
   uint8_t length_width_;
-  IntegerAlphabet run_length_alphabet_;
 
 };
 
@@ -83,43 +61,10 @@ void GrammarTrie::Insert (Iterator it, Iterator end)
       node->children[*it] = std::make_shared<Node>(*it);
     }
     node = node->children[*it];
+    ++(node->count);
     ++it;
   }
-  ++(node->count);
   node->lex_rank = 1;
-  return;
-}
-
-void GrammarTrie::CalculateCumulativeCountAndRank ()
-{
-  uint64_t lex_rank {};
-  std::deque<std::pair<std::shared_ptr<GrammarTrie::Node>, bool>> nodes;
-  nodes.emplace_back(root_, true);
-  while (!nodes.empty())
-  {
-    auto node {std::get<0>(nodes.back())};
-    auto& is_forward {std::get<1>(nodes.back())};
-    if (is_forward)
-    {
-      is_forward = false;
-      if (node->lex_rank)
-      {
-        node->lex_rank = ++lex_rank;
-      }
-      for (auto it {std::rbegin(node->children)}; it != std::rend(node->children); ++it)
-      {
-        nodes.emplace_back(std::get<1>(*it), true);
-      }
-    }
-    else
-    {
-      for (auto const& pair : node->children)
-      {
-        node->count += std::get<1>(pair)->count;
-      }
-      nodes.pop_back();
-    }
-  }
   return;
 }
 
@@ -129,7 +74,7 @@ std::ostream& operator<< (std::ostream& out, GrammarTrie const& grammar_trie)
   std::deque<std::pair<std::shared_ptr<GrammarTrie::Node>, uint64_t>> nodes;
   nodes.emplace_back(grammar_trie.root_, 0);
   uint64_t size {};
-  uint64_t divisor {1ULL << grammar_trie.GetLengthWidth()};
+  uint64_t divisor {1ULL << grammar_trie.length_width_};
   while (!nodes.empty())
   {
     ++size;
@@ -149,9 +94,7 @@ std::ostream& operator<< (std::ostream& out, GrammarTrie const& grammar_trie)
     }
   }
   out << "|nodes|:" << (size - 1) << "\n";
-  out << "length_width_:" << static_cast<uint64_t>(grammar_trie.GetLengthWidth()) << "\n";
-  out << "alphabet_width_:" << static_cast<uint64_t>(grammar_trie.GetAlphabetWidth()) << "\n";
-  out << "effective_alphabet_width_:" << static_cast<uint64_t>(grammar_trie.GetEffectiveAlphabetWidth()) << "\n";
+  out << "length_width:" << static_cast<uint64_t>(grammar_trie.length_width_) << "\n";
   return out;
 }
 }
