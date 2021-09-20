93b88c82ea216df2ef640aaea52517a365a8533a
jamie <jinjiedeng.jjd@gmail.com>
2021-09-20 15:52:44 +0800

    add Predecessor()

--- src/include/sparse_prefix_sum.h
+++ src/include/sparse_prefix_sum.h
@@ -25,7 +25,23 @@ public:
   );
   void Load (std::istream& in);
 
-  uint64_t operator[] (uint64_t const index) const;
+  inline uint64_t operator[] (uint64_t const index) const
+  {
+    if (index < std::size(prefix_sum_bits_))
+    {
+      return prefix_sum_select_1_(index + 1);
+    }
+    return std::size(prefix_sum_bits_);
+  }
+
+  inline uint64_t Predecessor (uint64_t const index) const
+  {
+    if (index < std::size(prefix_sum_bits_))
+    {
+      return prefix_sum_rank_1_(index);
+    }
+    return prefix_sum_rank_1_(std::size(prefix_sum_bits_));
+  }
 
   friend std::ostream& operator<< (std::ostream& out, SparsePrefixSum const& sparse_prefix_sum);
 
@@ -33,6 +49,7 @@ private:
 
   sdsl::sd_vector<> prefix_sum_bits_;
   sdsl::sd_vector<>::select_1_type prefix_sum_select_1_;
+  sdsl::sd_vector<>::rank_1_type prefix_sum_rank_1_;
 
 };
 
@@ -40,6 +57,7 @@ SparsePrefixSum::SparsePrefixSum (std::deque<uint64_t> const& prefix_sum)
 {
   prefix_sum_bits_ = decltype(prefix_sum_bits_)(std::begin(prefix_sum), std::end(prefix_sum));
   prefix_sum_select_1_.set_vector(&prefix_sum_bits_);
+  prefix_sum_rank_1_.set_vector(&prefix_sum_bits_);
 }
 
 SparsePrefixSum::SparsePrefixSum (SparsePrefixSum&& sparse_prefix_sum)
@@ -67,28 +85,22 @@ void SparsePrefixSum::Swap (SparsePrefixSum& sparse_prefix_sum)
     prefix_sum_bits_.swap(sparse_prefix_sum.prefix_sum_bits_);
     prefix_sum_select_1_.set_vector(&prefix_sum_bits_);
     sparse_prefix_sum.prefix_sum_select_1_.set_vector(&sparse_prefix_sum.prefix_sum_bits_);
+    prefix_sum_rank_1_.set_vector(&prefix_sum_bits_);
+    sparse_prefix_sum.prefix_sum_rank_1_.set_vector(&sparse_prefix_sum.prefix_sum_bits_);
   }
   return;
 }
 
-uint64_t SparsePrefixSum::operator[] (uint64_t const index) const
-{
-  if (index < std::size(prefix_sum_bits_))
-  {
-    return prefix_sum_select_1_(index + 1);
-  }
-  return std::size(prefix_sum_bits_);
-}
-
 std::ostream& operator<< (std::ostream& out, SparsePrefixSum const& sparse_prefix_sum)
 {
-  for (uint64_t i {1}, offset {}; offset != (std::size(sparse_prefix_sum.prefix_sum_bits_) - 1); ++i)
+  auto number_of_ones {sparse_prefix_sum.prefix_sum_rank_1_(std::size(sparse_prefix_sum.prefix_sum_bits_))};
+  for (uint64_t i {}; i != number_of_ones; ++i)
   {
-    offset = sparse_prefix_sum.prefix_sum_select_1_(i);
-    out << offset << ((offset != (std::size(sparse_prefix_sum.prefix_sum_bits_) - 1)) ? " " : "\n");
+    out << sparse_prefix_sum.prefix_sum_select_1_(i + 1) << ((i != (number_of_ones - 1)) ? " " : "\n");
   }
-  out << "space:\n";
-  out << ProperSizeRepresentation(sdsl::size_in_bytes(sparse_prefix_sum.prefix_sum_bits_)) << "B\n";
+  {
+    out << "prefix_sum_bits_:" << ProperSizeRepresentation(sdsl::size_in_bytes(sparse_prefix_sum.prefix_sum_bits_)) << "B\n";
+  }
   return out;
 }
 
@@ -103,13 +115,11 @@ uint64_t SparsePrefixSum::Serialize
   if (!parent)
   {
     sdsl::serialize(prefix_sum_bits_, out);
-    sdsl::serialize(prefix_sum_select_1_, out);
   }
   else
   {
     auto node {std::make_shared<SpaceNode>(name)};
     node->AddLeaf("prefix_sum_bits_", sdsl::serialize(prefix_sum_bits_, out));
-    node->AddLeaf("prefix_sum_select_1_", sdsl::serialize(prefix_sum_select_1_, out));
     parent->AddChild(node);
     size_in_bytes = node->GetSizeInBytes();
   }
@@ -119,8 +129,8 @@ uint64_t SparsePrefixSum::Serialize
 void SparsePrefixSum::Load (std::istream& in)
 {
   prefix_sum_bits_.load(in);
-  prefix_sum_select_1_.load(in);
   prefix_sum_select_1_.set_vector(&prefix_sum_bits_);
+  prefix_sum_rank_1_.set_vector(&prefix_sum_bits_);
   return;
 }
 }
